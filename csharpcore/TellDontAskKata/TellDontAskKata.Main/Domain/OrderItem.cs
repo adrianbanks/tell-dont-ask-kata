fe74079ec2857939b44e19e32ca777eb5d311f7b
Adrian Banks <adrian.banks@red-gate.com>
2022-10-14 14:25:55 +0100

    Convert Order to a record

--- csharpcore/TellDontAskKata/TellDontAskKata.Main/Domain/OrderItem.cs
+++ csharpcore/TellDontAskKata/TellDontAskKata.Main/Domain/OrderItem.cs
@@ -1,18 +1,4 @@
 ﻿namespace TellDontAskKata.Main.Domain
 {
-    public sealed class OrderItem
-    {
-        public Product Product { get; }
-        public int Quantity { get; }
-        public decimal TaxedAmount { get; }
-        public decimal Tax { get; }
-
-        public OrderItem(Product product, int quantity, decimal taxedAmount, decimal tax)
-        {
-            Product = product;
-            Quantity = quantity;
-            TaxedAmount = taxedAmount;
-            Tax = tax;
-        }
-    }
+    public record OrderItem(Product Product, int Quantity, decimal TaxedAmount, decimal Tax);
 }
