452244a5a8b94a45306ea3e62fbd454cb8e05c84
Adrian Banks <adrian.banks@red-gate.com>
2022-10-14 14:18:27 +0100

    Make Product immutable

--- csharpcore/TellDontAskKata/TellDontAskKata.Main/Domain/Product.cs
+++ csharpcore/TellDontAskKata/TellDontAskKata.Main/Domain/Product.cs
@@ -1,9 +1,16 @@
 ﻿namespace TellDontAskKata.Main.Domain
 {
-    public class Product
+    public sealed class Product
     {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public Category Category { get; set; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public Category Category { get; }
+
+        public Product(string name, decimal price, Category category)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+        }
     }
 }
